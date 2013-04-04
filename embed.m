692063ebdf4ac8d5f93fd04375f9a2f909b10033
Grzegorz Adam Hankiewicz <gradha@imap.cc>
2013-04-04 22:25:27 +0200

    Splits into separate file to pragma compile and use from C.

---
+++ embed.m
@@ -0,0 +1,23 @@
+#import <AppKit/AppKit.h>
+
+// Returns zero on success, non zero on failure.
+int macosxNimRecycle(const char *filename)
+{
+  NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+  NSString *path = [[NSString alloc] initWithUTF8String:filename];
+  NSString *folder = [path stringByDeletingLastPathComponent];
+  NSArray *files = [NSArray arrayWithObject:[path lastPathComponent]];
+
+  NSInteger tag = 0;
+  const BOOL ret = [[NSWorkspace sharedWorkspace]
+    performFileOperation:NSWorkspaceRecycleOperation
+    source:folder destination:nil files:files tag:&tag];
+  [pool release];
+
+  if (tag < 0)
+    return tag;
+  else if (ret == NO)
+   return 1;
+  else
+    return 0;
+}
--- recycle.nim
+++ recycle.nim
@@ -1,31 +1,7 @@
 import strutils, os
 
 {.passL: "-framework AppKit".}
-{.emit: """
-#import <AppKit/AppKit.h>
-
-// Returns zero on success, non zero on failure.
-int macosxNimRecycle(const char *filename)
-{
-  NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-  NSString *path = [[NSString alloc] initWithUTF8String:filename];
-  NSString *folder = [path stringByDeletingLastPathComponent];
-  NSArray *files = [NSArray arrayWithObject:[path lastPathComponent]];
-
-  NSInteger tag = 0;
-  const BOOL ret = [[NSWorkspace sharedWorkspace]
-    performFileOperation:NSWorkspaceRecycleOperation
-    source:folder destination:nil files:files tag:&tag];
-  [pool release];
-
-  if (tag < 0)
-    return tag;
-  else if (ret == NO)
-   return 1;
-  else
-    return 0;
-}
-""".}
+{.compile: "embed.m".}
 
 proc macosxNimRecycle(filename: cstring): int {.importc, nodecl.}
 
