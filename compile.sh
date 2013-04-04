faa09d368ea28dcaa37dd559d52ace955075de47
Grzegorz Adam Hankiewicz <gradha@imap.cc>
2013-04-04 21:20:43 +0200

    Definitive version, appkit is supported and performant.

--- compile.sh
+++ compile.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-gcc -o recycle -framework CoreServices -framework Cocoa recycle.m
+#gcc -o recycle -framework CoreServices -framework Cocoa recycle.m
+gcc -o recycle -framework AppKit recycle.m
 touch test1.todelete something.todelete test2.todelete
 ln -s something.todelete link.todelete
--- recycle.m
+++ recycle.m
@@ -1,36 +1,39 @@
 #include <stdlib.h>
+#include <stdio.h>
 
-#import <Foundation/Foundation.h>
-#include <CoreServices/CoreServices.h>
+#import <AppKit/AppKit.h>
 
 void recycle(const char *filename)
 {
-	NSString* path = [[NSString alloc] initWithUTF8String:filename];
-	OSStatus status = 0;
+	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+	NSString *path = [[NSString alloc] initWithUTF8String:filename];
+	NSString *folder = [path stringByDeletingLastPathComponent];
+	NSArray *files = [NSArray arrayWithObject:[path lastPathComponent]];
 
-	FSRef ref;
-	status = FSPathMakeRefWithOptions(
-		(const UInt8 *)[path fileSystemRepresentation], 
-		kFSPathMakeRefDoNotFollowLeafSymlink,
-		&ref, 
-		NULL
-	);  
-	[path release];
-	if (status) {
-		printf("failed to make FSRef, status %d\n", status);
-		return;
+	NSInteger tag = 0;
+	const BOOL ret = [[NSWorkspace sharedWorkspace]
+		performFileOperation:NSWorkspaceRecycleOperation
+		source:folder destination:nil files:files tag:&tag];
+	[pool release];
+	if (tag < 0 || ret == NO) {
+		printf("Errors deleting %s\n", filename);
+	} else {
+		//printf("Yeah\n");
 	}
-
-	status = FSMoveObjectToTrashSync(&ref, NULL, kFSFileOperationSkipPreflight);
-	printf("status: %i\n", (int)status);
 }
 
 int main(int argc, char **argv)
 {
+	char buf[100];
 	int f;
-	for (f = 1; argv[f]; f++) {
-		printf("%s\n", argv[f]);
-		recycle(argv[f]);
+
+	for (f = 0; f < 1000; f++) {
+		sprintf(buf, "onetest%d.todelete", f);
+		mkdir(buf, 0777);
+	}
+	for (f = 0; f < 1000; f++) {
+		sprintf(buf, "onetest%d.todelete", f);
+		recycle(buf);
 	}
 
 	return 0;
