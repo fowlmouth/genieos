1657e33af98542381cb7d8c8d12762a7edebe3ec
Grzegorz Adam Hankiewicz <gradha@imap.cc>
2013-11-29 19:21:52 +0100

    Prepares version number for 9.2.0 release.

--- genieos.nim
+++ genieos.nim
@@ -24,8 +24,8 @@ type
     defaultBeep, recycleBin
 
 const
-  VERSION_STR* = "9.1.2" ## Module version as a string.
-  VERSION_INT* = (major: 9, minor: 1, maintenance: 2) ## \
+  VERSION_STR* = "9.2.0" ## Module version as a string.
+  VERSION_INT* = (major: 9, minor: 2, maintenance: 0) ## \
   ## Module version as an integer tuple.
   ##
   ## Major versions changes mean a break in API backwards compatibility, either
